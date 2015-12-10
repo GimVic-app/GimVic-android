2e2cd04cd3c3e523f76546c6452b435c3a62122c
Ziga Patacko Koderman <ziga.patacko@gmail.com>
2015-12-10 17:35:47 +0100

    server address replaced

--- app/src/main/java/com/zigapk/gimvic/suplence/Configuration.java
+++ app/src/main/java/com/zigapk/gimvic/suplence/Configuration.java
@@ -4,6 +4,6 @@ package com.zigapk.gimvic.suplence;
  * Created by zigapk on 10.12.2015.
  */
 public class Configuration {
-    public static final String server = "http://vicstock.404.si";
+    public static final String server = "http://gimvicapp.404.si";
     public static final int PERMISSIONS_WRITE_EXTERNAL_STORAGE = 1;
 }
